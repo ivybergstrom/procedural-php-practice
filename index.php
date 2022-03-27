2129e8a6fa88259963d0e94a18525eca448eaddd
Ivy B <80648381+ivybergstrom@users.noreply.github.com>
2022-03-27 09:28:42 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> This is a paragraph </p.>
+    
+    <?php
+        echo "Hi tere. This is also a paragraph!";
+        echo 'I\'m using an example of escaping a quote.'
+        $a = true;
+        if($a === true) {
+            echo "Congrats my dude, there is truth here."
+        }
+    //if a page is made entirely of php do not use a closing tag!
+    ?> 
+    
+    
+    <p> This is a third paragraph. </p>
+
+
+
+</body>
+</html>
